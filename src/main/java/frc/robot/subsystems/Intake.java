f42ed3ae0b814ee6a16de958d377ebba1d36f1fa
Bennett Petzold <bennettpetzold01@gmail.com>
2021-01-11 17:23:43 -0500

    Add intake javadocs

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,10 +6,18 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+/**
+ * Provides an interface for the Power Cell intake mechanism.
+ */
 public class Intake extends SubsystemBase{
     
     private CANSparkMax intakeMotor;
     
+    /**
+     * Creates a new Intake and prepares its motor.
+     * 
+     * @see Intake
+     */
     public Intake(){
 
         super();
@@ -19,7 +27,13 @@ public class Intake extends SubsystemBase{
         intakeMotor.setInverted(true);
 
     }
-
+    /**
+     * Sets a new angular velocity to run the motor at.
+     * Ranges from -1.0 to 1.0, with 0 being no rotation.
+     * For expected behavior (positive values taking in balls), the sign is flipped.
+     *
+     * @param speed the intended direction (sign) and percent of maximum motor speed (0 to 1) 
+     */
     public void run_intake(double speed) {
         if (intakeMotor != null) {
             intakeMotor.set(-speed);
