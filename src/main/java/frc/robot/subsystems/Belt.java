a5cc567b09b53538f8388946b3ea63451ad438c1
Bennett Petzold <bennettpetzold01@gmail.com>
2021-01-11 17:02:58 -0500

    Add javadocs for Belt

--- src/main/java/frc/robot/subsystems/Belt.java
+++ src/main/java/frc/robot/subsystems/Belt.java
@@ -6,10 +6,18 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+/**
+ * Provides an interface for the conveyor belt, which holds balls and loads the flywheel.
+ */
 public class Belt extends SubsystemBase{
     
     private CANSparkMax beltMotor;
     
+    /**
+     * Creates a new Belt and sets up the belt motor.
+     * 
+     * @see Belt
+     */
     public Belt(){
         super();
 
@@ -18,6 +26,11 @@ public class Belt extends SubsystemBase{
         beltMotor.setInverted(true);
     }
 
+    /**
+     * Sets a new angular velocity to run the motor at.
+     * Ranges from -1.0 to 1.0, with 0 being no rotation.
+     * @param speed the intended direction (sign) and percent of maximum motor speed (0 to 1) 
+     */
     public void run_belt(double speed) {
         beltMotor.set(speed);
     }
