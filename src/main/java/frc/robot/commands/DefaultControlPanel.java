cd9ac37ca2b806f200a6f2d9e888a3f839d8b856
Bennett Petzold <bennettpetzold01@gmail,com>
2021-01-10 21:06:57 -0500

    Improve DefaultControlPanel documentation

--- src/main/java/frc/robot/commands/DefaultControlPanel.java
+++ src/main/java/frc/robot/commands/DefaultControlPanel.java
@@ -12,13 +12,21 @@ import java.util.function.DoubleSupplier;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ControlPanel;
 
-//Spin the control panel
+/**
+ * Spins the Control Panel to achieve Rotation Control.
+ * <p>
+ * See game manual section: 3.5.1 Control Panel
+ */
 public class DefaultControlPanel extends CommandBase {
   private final ControlPanel m_controlPanel;
   private final DoubleSupplier m_axis; 
 
   /**
-   * Creates a new ControlPanelRevolutions.
+   * Creates a new DefaultControlPanel.
+   * 
+   * @param controlPanel an instance of the control panel subsystem
+   * @param axis the position of the left control stick
+   * @see DefaultControlPanel
    */
   public DefaultControlPanel(ControlPanel controlPanel, DoubleSupplier axis) {
     super();
@@ -28,34 +36,47 @@ public class DefaultControlPanel extends CommandBase {
     addRequirements(controlPanel);
   }
 
-  // Called when the command is initially scheduled.
+  /**
+   * Prepares to spin the Control Panel by extending upwards.
+   * Called when the command is intially scheduled.
+   */
   @Override
   public void initialize() {
     this.m_controlPanel.extend();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
+  /**
+   * Spins the Control Panel engaging wheel.
+   * Uses the left control stick to determine direction and speed.
+   * Retracts and stops wheel if the left control stick is resting.
+   */
   @Override
   public void execute() {
     if(m_axis.getAsDouble() != 0) {
-      // if the control panel axis is nonzero
       m_controlPanel.extend();
       m_controlPanel.setSpeed(m_axis.getAsDouble());
     } else {
-      // if the control panel is not being actuated, retract and stop the wheel
       m_controlPanel.retract();
       m_controlPanel.setSpeed(0);
     }
   }
 
-  // Called once the command ends or is interrupted.
+  /**
+   * Retracts and stops wheel on command cancellation or interruption.
+   * Cancellation and interruption have the same behavior.
+   */
   @Override
   public void end(boolean interrupted) {
     m_controlPanel.retract();
     m_controlPanel.setSpeed(0);
   }
 
-  // Returns true when the command should end.
+  /**
+   * As a default command, always returns false.
+   * 
+   * @see  edu.wpi.first.wpilibj2.command.Subsystem#setDefaultCommand
+   */
   @Override
   public boolean isFinished() {
     return false;
