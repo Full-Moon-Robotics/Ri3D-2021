36b8e3834396c299935aad77fac52934252488b9
Bennett Petzold <bennettpetzold01@gmail.com>
2021-01-11 14:36:27 -0500

    Add javadocs to ResetDrivePose

--- src/main/java/frc/robot/commands/ResetDrivePose.java
+++ src/main/java/frc/robot/commands/ResetDrivePose.java
@@ -3,15 +3,30 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Drivetrain;
 
+/**
+ * Sets drivetrain sensors to a default state.
+ * <p>
+ * Executes once before robot autonomous movement.
+ */
 public class ResetDrivePose extends InstantCommand {
     
     private Drivetrain m_drivetrain;
 
+    /**
+     * Creates a new ResetDrivePose.
+     * @param dt The robot's drivetrain instance to be reset.
+     * @see ResetDrivePose
+     */
     public ResetDrivePose(Drivetrain dt) {
         super();
         m_drivetrain = dt;
     }
 
+    /**
+     * Executes the reset exactly once and within a single cycle.
+     * 
+     * @see edu.wpi.first.wpilibj2.command.InstantCommand
+     */
     @Override
     public void execute() {
         m_drivetrain.resetPose();
